c81e0fa2a3ac202ca17108b52dea822dbf1b2f84
GakutaroNishimura <nishi.g@quark.ap.titech.ac.jp>
2023-12-29 12:59:36 +0900

    129Xe enrich omega_p

--- PseudoMagDraw.py
+++ PseudoMagDraw.py
@@ -6,9 +6,9 @@ import ImRe_func as func
 
 B_0 = 1.5*10**(-3) #[T]
 E_0 = 9.57 #[eV]
-Pol129 = 0.02
+Pol129 = 0.06
 #x = np.linspace(10*10**(-3), 10.0, 10000)
-x = np.linspace(9.0, 10.0, 10000)
+x = np.linspace(5.0, 14.0, 10000)
 iB = np.linspace(-0.1, 0.1, 10000)
 #x = np.linspace(8.2, 11.0, 10000)
 
@@ -31,12 +31,12 @@ for ix in x:
 A_s_rate = [A_s_enrich[i]/A_s_enrich1[i] for i in range(len(A_s_enrich))]
 
 fig, ax = plt.subplots()
-ax.plot(x, func.Omega_zero(B_0) + x*0, label="$\omega_0$ [Hz]")
+# ax.plot(x, func.Omega_zero(B_0) + x*0, label="$\omega_0$ [Hz]")
 #ax.plot(x, func.Omega_pseud(x), label="$\omega_p$ [Hz]")
-ax.plot(x, func.Omega_prime(x, B_0, Pol129=0.01), label="$\omega_p'$ [Hz]")
+# ax.plot(x, func.Omega_prime(x, B_0, Pol129=0.06), label="$\omega_p'$ [Hz]")
 #ax.plot(iB, func.Omega_prime(E_0, iB, Pol129=0.01), label="$\omega_p'$ [Hz]")
 #ax.plot(x, func.B_pseudo(x), label="$B_p$ [T]")
-#ax.plot(x, func.n_rotation(x, B_0, Pol129), label=r"$\theta_{\mathrm{n}}$ [deg.]")
+ax.plot(x, func.n_rotation(x, B_0, Pol129), label=r"$\theta_{\mathrm{n}}$ [deg.]")
 
 #ax.plot(x, A_s_rate, label="rate")
 #ax.plot(x, A_s_enrich, label="129 enrich : 1 atm 10 cm")
@@ -46,12 +46,12 @@ ax.plot(x, func.Omega_prime(x, B_0, Pol129=0.01), label="$\omega_p'$ [Hz]")
 
 ax.set_xlabel("E [eV]")
 #ax.set_xlabel("$B_0$ [T]")
-#ax.set_ylabel("$\omega$ [Hz]")
+# ax.set_ylabel("$\omega$ [Hz]")
 #ax.set_ylabel("$\omega_p$ [Hz]")
-ax.set_ylabel("$A_s$")
+# ax.set_ylabel("$A_s$")
 #ax.set_ylabel("$B_p$ [T]")
 #ax.set_ylabel("$\omega_p'$ [Hz]")
-#ax.set_ylabel("neutron rotation [deg.]")
+ax.set_ylabel("neutron rotation [deg.]")
 plt.grid()
 plt.legend()
 fig.tight_layout()
