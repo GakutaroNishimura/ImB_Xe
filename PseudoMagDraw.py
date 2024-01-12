34893c1a75f9fabca5d21e6c8c731b814b683946
nishi <nishi.g@quark.ap.titech.ac.jp>
2024-01-12 19:21:12 +0900

    131XeのT_assymを書いてみた.

--- PseudoMagDraw.py
+++ PseudoMagDraw.py
@@ -18,6 +18,7 @@ A_s_enrich = []
 A_s_enrich1 = []
 A_s_enrich2 = []
 A_s_nat = []
+A_s_nat2 = []
 
 for ix in x:
     iA_s = func.Tasymm_analyzer(ix, Pol129, B_0, 1.0, 0.1)
@@ -34,6 +35,10 @@ for ix in x:
 for ix in x:
     iA_s = func.Tasymm_analyzer2(ix, Pol129, B_0, 1.0, 0.1)
     A_s_enrich2.append(iA_s)
+    
+for ix in x:
+    iA_s = func.Tasymm_analyzer2(ix, Pol131, B_0, 3.0*0.212, 0.1)
+    A_s_nat2.append(iA_s)
 
 # A_s_rate = [A_s_enrich[i]/A_s_enrich1[i] for i in range(len(A_s_enrich))]
 
@@ -52,14 +57,14 @@ for iB0 in B_list:
 # ax.plot(x, AsList[0], label="$B_0=%f$ [mT]"%0.0)
 # ax.plot(x, AsList[1], label="$B_0=%f$ [mT]"%1.5)
 # ax.plot(x, AsList[2], label="$B_0=%f$ [Hz]"%1.5)
-# ax.plot(x, func.Omega_zero(B_0) + x*0, label="$\omega_0$ [Hz]")
+ax.plot(x, func.Omega_zero(B_0) + x*0, label="$\omega_0$ [Hz]")
 #ax.plot(x, func.Omega_pseud(x), label="$\omega_p$ [Hz]")
 # ax.plot(x, func.Omega_prime(x, B_0, Pol129=0.06), label="$\omega_p'$ [Hz]")
-# ax.plot(x, func.Omega_prime131(x, B_0, Pol131), label="$\omega_p'$ [Hz]")
+ax.plot(x, func.Omega_prime131(x, B_0, Pol131), label="$\omega_p'$ [Hz]")
 #ax.plot(iB, func.Omega_prime(E_0, iB, Pol129=0.01), label="$\omega_p'$ [Hz]")
 #ax.plot(x, func.B_pseudo(x), label="$B_p$ [T]")
 # ax.plot(x, func.n_rotation(x, B_0, Pol129), label=r"$\theta_{\mathrm{n}}$ [deg.]")
-ax.plot(x, func.n_rotation131(x, B_0, Pol131), label=r"$\theta_{\mathrm{n}}$ [deg.]")
+# ax.plot(x, func.n_rotation131(x, B_0, Pol131), label=r"$\theta_{\mathrm{n}}$ [deg.]")
 
 #ax.plot(x, A_s_rate, label="rate")
 # ax.plot(x, A_s_enrich, label="129 enrich : 1 atm 10 cm")
@@ -67,7 +72,9 @@ ax.plot(x, func.n_rotation131(x, B_0, Pol131), label=r"$\theta_{\mathrm{n}}$ [de
 # ax.plot(x, A_s_enrich2)
 #ax.plot(x, A_s_enrich, label="129 enrich : 1% pol")
 #ax.plot(x, A_s_enrich1, label="129 enrich : 2% pol")
-#ax.plot(x, A_s_nat, label="natural : 3 atm 5 cm")
+# ax.plot(x, A_s_nat, label="natural : 3 atm 5 cm")
+# ax.plot(x, A_s_nat2, label="131Xe")
+
 
 ax.set_xlabel("E [eV]")
 #ax.set_xlabel("$B_0$ [T]")
