7ae034ca2f68177ccab4cbfbeb051ec9a5fe106e
nishi <nishi.g@quark.ap.titech.ac.jp>
2024-02-02 18:50:07 +0900

    Tasymのsinx~xじゃない場合も見てみた.

--- PseudoMagDraw.py
+++ PseudoMagDraw.py
@@ -6,18 +6,18 @@ import ImRe_func as func
 import const
 
 
-B_0 = 1.5*10**(-3) #[T]
+B_0 = 0.4*10**(-3) #[T]
 # B_0 = 0.0
 B_list = [0.0, 1.5e-3]
 E_0 = 9.57 #[eV]
 Pol129 = 0.06
-Pol131 = 0.04*10**(-2)
-# Pol131 = 7.6*10**(-2) #Mike
+# Pol131 = 0.04*10**(-2)
+Pol131 = 7.6*10**(-2) #Mike
 # Pol131 = 1.0
 #x = np.linspace(10*10**(-3), 10.0, 10000)
-x = np.linspace(2.0, 20.0, 10000)
+# x = np.linspace(2.0, 20.0, 10000)
 # x = np.linspace(5.0, 14.0, 10000)
-# x = np.linspace(1.2, 5.2, 10000)
+x = np.linspace(1.2, 5.2, 10000)
 d = np.linspace(0.0, 30.0, 10000)
 iB = np.linspace(-0.1, 0.1, 10000)
 # x = np.linspace(5.0, 14.0, 10000)
@@ -27,6 +27,8 @@ A_s_enrich1 = []
 A_s_enrich2 = []
 A_s_nat = []
 A_s_nat2 = []
+A_s_nat3 = []
+A_s_nat4 = []
 A_s_Npol100 = []
 
 A_s_Mike0 = []
@@ -53,9 +55,17 @@ Nlist = []
 #     iA_s = func.Tasymm_analyzer2(ix, Pol129, B_0, 1.0, const.d_cell)
 #     A_s_enrich2.append(iA_s)
 
-for ix in x:
-    iA_s = func.Tasymm_analyzer2(ix, Pol131, B_0, 3.0*0.212, const.d_cell)
-    A_s_nat2.append(iA_s)
+# for ix in x:
+#     iA_s = func.Tasymm_analyzer2(ix, Pol131, B_0, 3.0*0.212, const.d_cell)
+#     A_s_nat2.append(iA_s)
+    
+# for ix in x:
+#     iA_s = func.Tasymm_analyzer3(ix, Pol131, B_0, 3.0*0.212, const.d_cell)
+#     A_s_nat3.append(iA_s)
+    
+# for ix in x:
+#     iA_s = func.Tasymm_analyzer3(ix, Pol131, 0.0, 3.0*0.212, const.d_cell)
+#     A_s_nat4.append(iA_s)
 
 # for ix in x:
 #     iA_s = func.Tasymm_analyzer2_1(ix, Pol131, B_0, 3.0*0.212, const.d_cell)
@@ -66,30 +76,44 @@ for ix in x:
 #     jA_s = func.Tasymm_analyzer2_1(ix, Pol131, B_0, 3.0*0.212, const.d_cell)
 #     A_s = iA_s - jA_s
 #     A_s_nat2.append(A_s)
-    
-# for ix in x:
-#     iA_s = func.Tasymm_analyzer2(ix, Pol131, B_0, 300*0.84/760, const.d_cell*0)
-#     jA_s = func.Tasymm_analyzer2_1(ix, Pol131, B_0, 300*0.84/760, const.d_cell*0)
-#     A_s = iA_s - jA_s
-#     A_s_Mike0.append(A_s)
 
 # for ix in x:
-#     iA_s = func.Tasymm_analyzer2(ix, Pol131, B_0, 300*0.84/760, const.d_cell)
-#     jA_s = func.Tasymm_analyzer2_1(ix, Pol131, B_0, 300*0.84/760, const.d_cell)
+#     iA_s = func.Tasymm_analyzer3(ix, Pol131, B_0, 3.0*0.212, const.d_cell)
+#     jA_s = func.Tasymm_analyzer3_1(ix, Pol131, B_0, 3.0*0.212, const.d_cell)
 #     A_s = iA_s - jA_s
-#     A_s_Mike1.append(A_s)
+#     A_s_nat2.append(A_s)
     
-# for ix in x:
-#     iA_s = func.Tasymm_analyzer2(ix, Pol131, B_0, 300*0.84/760, const.d_cell*2)
-#     jA_s = func.Tasymm_analyzer2_1(ix, Pol131, B_0, 300*0.84/760, const.d_cell*2)
-#     A_s = iA_s - jA_s
-#     A_s_Mike2.append(A_s)
+for ix in x:
+    # iA_s = func.Tasymm_analyzer2(ix, Pol131, B_0, 300*0.84/760, const.d_cell*0)
+    # jA_s = func.Tasymm_analyzer2_1(ix, Pol131, B_0, 300*0.84/760, const.d_cell*0)
+    iA_s = func.Tasymm_analyzer3(ix, Pol131, B_0, 300*0.84/760, const.d_cell*0)
+    jA_s = func.Tasymm_analyzer3_1(ix, Pol131, B_0, 300*0.84/760, const.d_cell*0)
+    A_s = iA_s - jA_s
+    A_s_Mike0.append(A_s)
+
+for ix in x:
+    # iA_s = func.Tasymm_analyzer2(ix, Pol131, B_0, 300*0.84/760, const.d_cell)
+    # jA_s = func.Tasymm_analyzer2_1(ix, Pol131, B_0, 300*0.84/760, const.d_cell)
+    iA_s = func.Tasymm_analyzer3(ix, Pol131, B_0, 300*0.84/760, const.d_cell)
+    jA_s = func.Tasymm_analyzer3_1(ix, Pol131, B_0, 300*0.84/760, const.d_cell)
+    A_s = iA_s - jA_s
+    A_s_Mike1.append(A_s)
     
-# for ix in x:
-#     iA_s = func.Tasymm_analyzer2(ix, Pol131, B_0, 300*0.84/760, const.d_cell*3)
-#     jA_s = func.Tasymm_analyzer2_1(ix, Pol131, B_0, 300*0.84/760, const.d_cell*3)
-#     A_s = iA_s - jA_s
-#     A_s_Mike3.append(A_s)
+for ix in x:
+    # iA_s = func.Tasymm_analyzer2(ix, Pol131, B_0, 300*0.84/760, const.d_cell*2)
+    # jA_s = func.Tasymm_analyzer2_1(ix, Pol131, B_0, 300*0.84/760, const.d_cell*2)
+    iA_s = func.Tasymm_analyzer3(ix, Pol131, B_0, 300*0.84/760, const.d_cell*2)
+    jA_s = func.Tasymm_analyzer3_1(ix, Pol131, B_0, 300*0.84/760, const.d_cell*2)
+    A_s = iA_s - jA_s
+    A_s_Mike2.append(A_s)
+    
+for ix in x:
+    # iA_s = func.Tasymm_analyzer2(ix, Pol131, B_0, 300*0.84/760, const.d_cell*3)
+    # jA_s = func.Tasymm_analyzer2_1(ix, Pol131, B_0, 300*0.84/760, const.d_cell*3)
+    iA_s = func.Tasymm_analyzer3(ix, Pol131, B_0, 300*0.84/760, const.d_cell*3)
+    jA_s = func.Tasymm_analyzer3_1(ix, Pol131, B_0, 300*0.84/760, const.d_cell*3)
+    A_s = iA_s - jA_s
+    A_s_Mike3.append(A_s)
     
 # for ix in x:
 #     iA_s = func.Tasymm_analyzer2(ix, Pol131, B_0, 3.0, const.d_cell)
@@ -130,6 +154,7 @@ fig, ax = plt.subplots()
 # ax.plot(x, func.Omega_pseud(x, 1.0), label="$\omega_p$ [Hz]")
 # ax.plot(x, func.Omega_prime(x, B_0, Pol129=0.06), label="$\omega_p'$ [Hz]")
 # ax.plot(x, func.Omega_prime131(x, B_0, Pol131), label="$\omega_p'$")
+# ax.plot(x, func.Omega_prime131(x, B_0, Pol131), label="$\omega_p'$")
 # ax.plot(x, func.Omega_prime131_1(x, B_0, Pol131), label="$\omega_p'$ only p-wave")
 # ax.plot(x, func.Omega_pseud131(x, 3.0*0.212), label="$\omega_p'$ [Hz]")
 # ax.plot(x, func.Omega_pseud131_1(x, 3.0*0.212), label="$\omega_p'$ [Hz]")
@@ -137,6 +162,7 @@ fig, ax = plt.subplots()
 #ax.plot(x, func.B_pseudo(x), label="$B_p$ [T]")
 # ax.plot(x, func.n_rotation(x, B_0, Pol129), label=r"$\theta_{\mathrm{n}}$ [deg.]")
 # ax.plot(x, func.n_rotation131(x, B_0, Pol131), label=r"$\theta_{\mathrm{n}}$ [deg.]")
+# ax.plot(x, func.n_rotation131(x, B_0/2, Pol131), label=r"$\theta_{\mathrm{n}}$ [deg.]")
 
 #ax.plot(x, A_s_rate, label="rate")
 # ax.plot(x, A_s_enrich, label="129 enrich : 1 atm 10 cm")
@@ -145,12 +171,14 @@ fig, ax = plt.subplots()
 #ax.plot(x, A_s_enrich, label="129 enrich : 1% pol")
 #ax.plot(x, A_s_enrich1, label="129 enrich : 2% pol")
 # ax.plot(x, A_s_nat, label="natural : 3 atm 5 cm")
-ax.plot(x, A_s_nat2)
+# ax.plot(x, A_s_nat2, label="sinx=x")
+# ax.plot(x, A_s_nat3, label="sinx")
+# ax.plot(x, A_s_nat4, label="B_0=0")
 # ax.plot(x, A_s_Npol100)
-# ax.plot(x, A_s_Mike0, label=r"$d_{\mathrm{Xe}}$ = 0 cm")
-# ax.plot(x, A_s_Mike1, label=r"$d_{\mathrm{Xe}}$ = 5 cm")
-# ax.plot(x, A_s_Mike2, label=r"$d_{\mathrm{Xe}}$ = 10 cm")
-# ax.plot(x, A_s_Mike3, label=r"$d_{\mathrm{Xe}}$ = 15 cm")
+ax.plot(x, A_s_Mike0, label=r"$d_{\mathrm{Xe}}$ = 0 cm")
+ax.plot(x, A_s_Mike1, label=r"$d_{\mathrm{Xe}}$ = 5 cm")
+ax.plot(x, A_s_Mike2, label=r"$d_{\mathrm{Xe}}$ = 10 cm")
+ax.plot(x, A_s_Mike3, label=r"$d_{\mathrm{Xe}}$ = 15 cm")
 # ax.plot(d, Nlist)
 
 ax.yaxis.set_major_formatter(ptick.ScalarFormatter(useMathText=True))   # こっちを先に書くこと.
@@ -158,14 +186,14 @@ ax.ticklabel_format(style="sci", axis="y", scilimits=(0,0))   # 10^3（10の3乗
 ax.set_xlabel("E [eV]")
 # ax.set_xlabel("$d_{\mathrm{Xe}}$ [cm]")
 # ax.set_xlabel("$B_0$ [T]")
-# ax.set_ylabel("$\omega$ [Hz]")
+ax.set_ylabel("$\omega$ [Hz]")
 #ax.set_ylabel("$\omega_p$ [Hz]")
-ax.set_ylabel("$A_{\mathrm{asym}}$")
+# ax.set_ylabel("$A_{\mathrm{asym}}$")
 # ax.set_ylabel("Neutron Flux [n/s]")
 #ax.set_ylabel("$B_p$ [T]")
 #ax.set_ylabel("$\omega_p'$ [Hz]")
 # ax.set_ylabel("neutron rotation [deg.]")
 plt.grid()
-# plt.legend()
+plt.legend()
 fig.tight_layout()
 plt.show()
